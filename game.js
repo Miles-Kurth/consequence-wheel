cd29cea6b00d4164a843bfb57a79e05cf14abf9e
Miles Kurth <mkurth9612@student.pps.net>
2026-04-08 10:19:30 -0700

    1

--- game.js
+++ game.js
@@ -1,2 +1,5 @@
 import Color from "https://colorjs.io/dist/color.js";
 
+const startButton = document.createElement('button');
+startButton.textContent = "SPIN";
+document.body.appendChild(startButton);
