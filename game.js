83d7e5036457a0fc2b39f5a94031e14ae9d3bf87
Miles Kurth <mkurth9612@student.pps.net>
2026-04-09 22:38:25 -0700

    17

--- game.js
+++ game.js
@@ -36,8 +36,9 @@ var gameArea = {
     drawCircle : function() {
         this.context.beginPath();
         this.context.arc(CENTERX, CENTERY, RADIUS, 0, (2*pi)/options.length);
-        this.context.fillStyle() = "red";
         this.context.stroke();
+        this.context.fillStyle = "red";
+        this.context.fill();
     }
 }
 
