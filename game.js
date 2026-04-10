ae1b1dd5e1846feff4ed13ec13a866757617d13d
Miles Kurth <mkurth9612@student.pps.net>
2026-04-09 22:09:33 -0700

    7

--- game.js
+++ game.js
@@ -14,8 +14,8 @@ let time = 0;
 var gameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = canvasWidth;
-        this.canvas.height = canvasHeight;
+        this.canvas.width = 600;
+        this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
